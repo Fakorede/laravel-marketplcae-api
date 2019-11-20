a4d1575492ee17d2db87ef22c9ffd69ca41a22f8
fakorede <abiolafakorede@gmail.com>
2019-11-20 16:26:58 +0100

    created api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,8 +9,33 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+/**
+ * Users
+ */
+Route::resource('users', 'User\UserController')->except(['create', 'edit']);
+/**
+ * Buyers
+ */
+Route::resource('buyers', 'Buyer\BuyerController')->only(['index', 'show']);
+/**
+ * Sellers
+ */
+Route::resource('sellers', 'Seller\SellerController')->only(['index', 'show']);
+/**
+ * Products
+ */
+Route::resource('products', 'Product\ProductController')->only(['index', 'show']);
+/**
+ * Categories
+ */
+Route::resource('categories', 'Category\CategoryController')->except(['create', 'edit']);
+/**
+ * Transactions
+ */
+Route::resource('transactions', 'Transaction\TransactionController')->only(['index', 'show']);
