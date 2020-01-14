3c2af9cf14595183da94131cf2b147bd3b97b97e
fakorede <abiolafakorede@gmail.com>
2020-01-14 12:31:10 +0100

    Return sellers owning products belonging to category with specified ID

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,7 @@ Route::resource('products', 'Product\ProductController')->only(['index', 'show']
  */
 Route::resource('categories', 'Category\CategoryController')->except(['create', 'edit']);
 Route::resource('categories.products', 'Category\CategoryProductController')->only(['index']);
+Route::resource('categories.sellers', 'Category\CategorySellerController')->only(['index']);
 /**
  * Transactions
  */
