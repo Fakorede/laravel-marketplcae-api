a10986c7b12b98697a0e3e311723819511d58f0b
fakorede <abiolafakorede@gmail.com>
2020-01-17 21:44:16 +0100

    return buyers of specific  product

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,7 @@ Route::resource('sellers.transactions', 'Seller\SellerTransactionController')->o
  * Products
  */
 Route::resource('products', 'Product\ProductController')->only(['index', 'show']);
+Route::resource('products.buyers', 'Product\ProductBuyerController')->only(['index']);
 Route::resource('products.transactions', 'Product\ProductTransactionController')->only(['index']);
 /**
  * Categories
