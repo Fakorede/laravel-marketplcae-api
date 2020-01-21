ce602cee7a04bd3265e95a64762c15a1e588ce22
fakorede <abiolafakorede@gmail.com>
2020-01-21 22:42:03 +0100

    using laravel markdown mailables

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,5 +1,12 @@
-Hello {{ $user->name }},
+@component('mail::message')
+# Hello {{ $user->name }},
 
 You recently requested for a change of email address. If this was you, please verify your new email using the link below:
 
-{{ route('verify', $user->verification_token) }}
+@component('mail::button', ['url' => route('verify', $user->verification_token) ])
+Verify Account
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,5 +1,12 @@
-Hello {{ $user->name }},
+@component('mail::message')
+# Hello {{ $user->name }},
 
 Thank you for creating an account with us. Please verify your email using the link below:
 
-{{ route('verify', $user->verification_token) }}
+@component('mail::button', ['url' => route('verify', $user->verification_token) ])
+Verify Account
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
